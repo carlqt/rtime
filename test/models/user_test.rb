3f34eec04fe58da3a5b5180ce2102fdc47a6cc53
Carl Tablante <carl.q.tablante@gmail.com>
2014-11-23 14:51:10 +0800

    added users model

---
+++ test/models/user_test.rb
@@ -0,0 +1,22 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id                    :integer          not null, primary key
+#  username              :string(255)
+#  email                 :string(255)
+#  contact               :string(255)
+#  company               :string(255)
+#  created_at            :datetime
+#  updated_at            :datetime
+#  password              :string(255)
+#  password_confirmation :string(255)
+#
+
+require 'test_helper'
+
+class UserTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
